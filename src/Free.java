3f607b6521c862ca2ae0b8d0d2cb6a6173167c30
Igor <test@test.com>
2015-11-05 20:51:26 +0200

    foreach

--- src/Free.java
+++ src/Free.java
@@ -2,24 +2,19 @@
 public class Free {
 
     public static void main(String[] args) {
-//        int i = 10;
-//        int[] arr;
-//        arr = new int[]{1, 2, 3};
-//        int[] test = new int[20];
-//        System.out.println(arr.length);
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 12; j++) {
-                if (i % 2 == 0 && j % 2 == 0) {
-                    System.out.print("*");
-                } else if (i % 2 != 0 && j%2 != 0) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        //int i = 10;
+        int[] arr;
+        arr = new int[]{1, 2, 3};
+        //int[] test = new int[20];
+        System.out.println(arr.length);
 
-                }
-            }
-            System.out.println("\n");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k]);
+        }
+
+        for(int k : arr) //foreach
+        {
+            System.out.println(k);
         }
     }
-
 }
