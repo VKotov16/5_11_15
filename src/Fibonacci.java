a3f6eeabebb9d17c68731aeaa7d8192b321a14d2
Igor <test@test.com>
2015-11-09 21:06:58 +0200

    recursia1

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,19 +2,41 @@
 
 public class Fibonacci {
 
+    public static long[] cache = new long[51];
+
     public static void main(String[] args) {
-        //System.out.println(Fib(50));
-        System.out.println(sum(11426));//11426 не валится, 11427 валится
+        System.out.println(fib1(50));
+        //System.out.println(sum(11426));//11426 не валится, 11427 валится
 
     }
 
-    public static long Fib(long num)
+
+
+    public static long fib1(int num)
     {
-        if(num == 1 || num == 2)
+        if(cache[num] !=0 )
+        {
+            return cache[num];
+        }
+        else if(num == 0 || num == 1)
+        {
+         cache[num] = 1;
+            return 1;
+        }
+        else {
+            long res = fib(num - 2) + fib(num - 1);
+            cache[num] = res;
+            return res;
+        }
+    }
+
+    public static long fib(long num)
+    {
+        if(num == 0 || num == 1)
             return 1;
         else
         {
-            return Fib(num - 2) + Fib(num-1);
+            return fib(num - 2) + fib(num - 1);
         }
     }
 
